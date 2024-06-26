36bb0a23ce4bca0ebfd19ba9a2e8f6f5798dac94
Rafael Levi <rafaellevicodur2@gmail.com>
2024-06-25 22:40:13 -0300

    update dashbord

---
+++ app/app.py
@@ -0,0 +1,86 @@
+import os
+import pandas as pd
+import streamlit as st
+from sqlalchemy import create_engine
+from sqlalchemy.exc import ProgrammingError
+from dotenv import load_dotenv
+
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Obter as variáveis do arquivo .env
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+# Criar a URL de conexão do banco de dados
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Criar o engine de conexão com o banco de dados
+engine = create_engine(DATABASE_URL)
+
+def get_data():
+    query = f"""
+    SELECT
+        *
+    FROM
+        public.dm_commodities;
+    """
+    df = pd.read_sql(query, engine)
+    return df
+
+# Configurar a página do Streamlit
+st.set_page_config(page_title='Dashboard do diretor', layout='wide')
+
+# Título do Dashboard
+st.title('Esse e um texto')
+
+# Descrição
+st.write("""
+Este dashboard mostra os dados de commodities e suas transações.
+""")
+
+# Obter os dados
+df = get_data()
+
+st.dataframe(df)
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
